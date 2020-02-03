039898918537018a3229a76370323547daa1610f
Julian Pereira <bleushade@gmail.com>
2020-02-03 23:42:56 +0800

    task(): add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,67 @@
-import React, { lazy, Suspense, FunctionComponent } from 'react';
-import { Container, Row, Col, Navbar, NavbarBrand } from 'reactstrap';
+import React, { lazy, Suspense, FunctionComponent, useState } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 import CalendarLoader from './components/loaders/CalendarLoader';
 import ErrorBoundary from './components/error-boundary/ErrorBoundary';
+import {
+  NavLink as ReactRouterNavLink,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 // import styles from './App.module.scss';
 
 const Calendar = lazy(() => import('./components/calendar/Calendar'));
 
 const App: FunctionComponent = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <ErrorBoundary>
-      <Navbar color="dark" light expand="lg">
-        <NavbarBrand href="/">romcal</NavbarBrand>
-      </Navbar>
-      <Container>
-        <Row>
-          <Col>
-            <Suspense fallback={<CalendarLoader />}>
-              <Calendar />
-            </Suspense>
-          </Col>
-        </Row>
-      </Container>
+      <Router>
+        <Navbar color="dark" light expand="lg">
+          <NavbarBrand href="/">romcal</NavbarBrand>
+          <NavbarToggler
+            onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) =>
+              setIsOpen(!isOpen)
+            }
+          />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink tag={ReactRouterNavLink} exact to="/calendar">
+                  Calendar
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col>
+              <Switch>
+                <Route path="/calendar">
+                  <Suspense fallback={<CalendarLoader />}>
+                    <Calendar />
+                  </Suspense>
+                </Route>
+                <Route path="/"></Route>
+              </Switch>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </ErrorBoundary>
   );
 };
